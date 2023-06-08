4e717b99d9ca237cf11842fc2fddeadf75d17f6d
MoonJaeSeung <wotmdskfk32@naver.com>
2023-06-08 14:21:18 +0900

    feat: 로그인 기능

--- src/main/java/com/shop/shop/controller/MemberController.java
+++ src/main/java/com/shop/shop/controller/MemberController.java
@@ -46,13 +46,14 @@ public class MemberController {
         return "redirect:/";
     }
 
-//    @PostMapping(value = "/new")
-//    public String memberForm(MemberFormDto memberFormDto){
-//
-//        Member member = Member.createMember(memberFormDto, passwordEncoder);
-//        memberService.saveMember(member);
-//
-//        return "redirect:/";
-//    }
+    @GetMapping(value="/login")
+    public String loginMember(){
+        return "/member/memberLoginForm";
+    }
 
+    @GetMapping(value = "/login/error")
+    public String loginError(Model model){
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
+        return "/member/memberLoginForm";
+    }
 }
